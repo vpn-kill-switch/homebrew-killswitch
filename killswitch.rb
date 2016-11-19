9fc6e50a76aa1a1865a5880c3a03962020d8f71f
nbari <nbari@tequila.io>
2016-11-19 12:31:31 +0100

    version 0.2.1

--- killswitch.rb
+++ killswitch.rb
@@ -1,9 +1,9 @@
 class Killswitch < Formula
   desc "create & load a kill-switch pf.conf"
   homepage "https://vpn-kill-switch.com/"
-  url "https://bintray.com/nbari/killswitch/download_file?file_path=killswitch_0.2.0_darwin_amd64.zip"
-  sha256 "9437f5eb3d03f68ec123a6bdddbcc43e8b5e6c8ac3224527d83921db9dc7b17c"
-  version "0.2.0"
+  url "https://bintray.com/nbari/killswitch/download_file?file_path=killswitch_0.2.1_darwin_amd64.zip"
+  sha256 "c037cc2431da8795db80e603790b6debbf7427e65a7b9c9689c7267e17a6e894"
+  version "0.2.1"
 
   def install
     bin.install "killswitch"
