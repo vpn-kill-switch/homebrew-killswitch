5e07eada4f3aefbe3b8d87daf618fb6075b2d681
nbari <nbari@tequila.io>
2017-03-21 14:17:13 +0100

    build instead of fetch binary 	modified:   killswitch.rb

--- killswitch.rb
+++ killswitch.rb
@@ -1,11 +1,31 @@
 class Killswitch < Formula
   desc "create & load a kill-switch pf.conf"
   homepage "https://vpn-kill-switch.com/"
-  url "https://bintray.com/nbari/killswitch/download_file?file_path=killswitch_0.2.2_darwin_amd64.zip"
-  sha256 "24d4285f525e42b894a6df17d9824dedaaa404d3253d88398929a70c973beee7"
-  version "0.2.2"
+  url "https://github.com/vpn-kill-switch/killswitch.git",
+      :tab => "0.2.2",
+      :revision => "fcf5a53b5ed9c56c75a91190665fa946f783ac96"
+
+  head "https://github.com/vpn-kill-switch/killswitch.git"
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "179e07bb22e2755e3d082f07607cf2e98e266defaace3ceb764a4c32f03ccf8d" => :sierra
+    sha256 "5be8ef01a8568b7de75b19d525a4830b0c934c43b9e8f9fb150cbdcb18091a26" => :el_capitan
+    sha256 "6dcf9d7de2489f5e3d6fd56488c3abe2bff1ea6f6bf0d9de89421b0e9a7e2e38" => :yosemite
+  end
+
+  depends_on "go" => :build
 
   def install
-    bin.install "killswitch"
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/vpn-kill-switch/killswitch").install buildpath.children
+    cd "src/github.com/vpn-kill-switch/killswitch" do
+      system "make"
+      bin.install "killswitch"
+    end
+  end
+
+  test do
+    system bin/"killswitch", "-v"
   end
 end
