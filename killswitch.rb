544e1f2ecf4af8728a93455aa00d6fc4f18d2b1d
nbari <nbari@tequila.io>
2017-11-20 21:41:54 +0100

    0.5.0

--- killswitch.rb
+++ killswitch.rb
@@ -1,37 +1,26 @@
-require "language/go"
-
 class Killswitch < Formula
   desc "create & load a kill-switch pf.conf"
   homepage "https://vpn-kill-switch.com/"
   url "https://github.com/vpn-kill-switch/killswitch.git",
-      :tag => "0.4.2",
-      :revision => "763f87a995141fef1372dd8dff8f57ee28baeded"
+      :tag => "0.5.0",
+      :revision => "0bdd5f06a34134869218a2d46cbbc575f4064db1"
   head "https://github.com/vpn-kill-switch/killswitch.git"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "179e07bb22e2755e3d082f07607cf2e98e266defaace3ceb764a4c32f03ccf8d" => :sierra
-    sha256 "5be8ef01a8568b7de75b19d525a4830b0c934c43b9e8f9fb150cbdcb18091a26" => :el_capitan
-    sha256 "6dcf9d7de2489f5e3d6fd56488c3abe2bff1ea6f6bf0d9de89421b0e9a7e2e38" => :yosemite
+    sha256 "af1a49059071713a330692106f9309911c398d375c425481702b812406e01547" => :high_sierra
+    sha256 "2744498ed27a3e7401cf8905a01498e10cb898d8e6786763907f59f64c1cac75" => :sierra
+    sha256 "33cf0dae84b792e44e256a9fec29e08e0d0ae3ad2598470f0b2328be200b3adc" => :el_capitan
   end
 
+  depends_on "dep" => :build
   depends_on "go" => :build
-
-  go_resource "github.com/miekg/dns" do
-    url "https://github.com/miekg/dns.git",
-        :revision => "e4205768578dc90c2669e75a2f8a8bf77e3083a4"
-  end
-
-  go_resource "golang.org/x/net" do
-    url "https://go.googlesource.com/net.git",
-        :revision => "1c05540f6879653db88113bc4a2b70aec4bd491f"
-  end
 
   def install
     ENV["GOPATH"] = buildpath
     (buildpath/"src/github.com/vpn-kill-switch/killswitch").install buildpath.children
-    Language::Go.stage_deps resources, buildpath/"src"
     cd "src/github.com/vpn-kill-switch/killswitch" do
+      system "dep", "ensure"
       ldflags = "-s -w -X main.version=#{version}"
       system "go", "build", "-ldflags", ldflags, "-o", "#{bin}/killswitch", "cmd/killswitch/main.go"
     end
