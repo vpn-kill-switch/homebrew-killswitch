9306b5806bbd5c1d4d58ce575cb7db04e0b63bbc
Karun Japhet <javatarz@users.noreply.github.com>
2019-10-07 11:48:08 +0530

    Update doctor's comments on formula (#3)
    * Update doctor's comments on formula
    
    Will help #20 when this repo gets more stars.
    
    * Reset formula name to Killswitch

--- killswitch.rb
+++ killswitch.rb
@@ -1,9 +1,7 @@
 class Killswitch < Formula
-  desc "create & load a kill-switch pf.conf"
+  desc "Create & load a kill-switch pf.conf"
   homepage "https://vpn-kill-switch.com/"
-  url "https://github.com/vpn-kill-switch/killswitch.git",
-      :tag => "0.7.0",
-      :revision => "b8e11177666f92608020133b07978ce09d0b615e"
+  url "https://github.com/vpn-kill-switch/killswitch.git", :tag => "0.7.0", :revision => "b8e11177666f92608020133b07978ce09d0b615e"
   head "https://github.com/vpn-kill-switch/killswitch.git"
 
   bottle do
@@ -20,7 +18,7 @@ class Killswitch < Formula
     ENV["GOPATH"] = buildpath
     (buildpath/"src/github.com/vpn-kill-switch/killswitch").install buildpath.children
     cd "src/github.com/vpn-kill-switch/killswitch" do
-      system "dep", "ensure"
+      system "dep", "ensure", "-vendor-only"
       ldflags = "-s -w -X main.version=#{version}"
       system "go", "build", "-ldflags", ldflags, "-o", "#{bin}/killswitch", "cmd/killswitch/main.go"
     end
